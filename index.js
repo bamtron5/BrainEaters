e8353337a397860ae1c668ee40737b30821af6b9
bamtron5 <brandon@brandonam.com>
2017-02-17 13:13:10 -0800

    boundary detection and height/width for imagery.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+var unit = 50;
 var c = document.getElementById('myCanvas');
 var context = c.getContext('2d');
-var img = new Image();
+var playerOne = new Image();
+var zW = unit;
+var zH = unit;
 var zX = 0;
 var zY = 0;
-img.onload = animate;
-img.src = "zombie.gif";
-img.addEventListener('load', function () {
-    context.drawImage(img, zX, zY);
+var bW = unit;
+var bH = unit;
+var bX = c.width - bW;
+var bY = c.height - bH;
+playerOne.src = "zombie.gif";
+playerOne.width = zW;
+playerOne.height = zH;
+playerOne.addEventListener('load', function () {
+    context.drawImage(playerOne, zX, zY, zW, zH);
+    makeAWall();
 });
 document.addEventListener('keydown', function (event) {
     switch (event.key) {
@@ -24,24 +33,51 @@ document.addEventListener('keydown', function (event) {
             break;
     }
 }, true);
-function animate() {
+function animate(content) {
     context.clearRect(0, 0, c.width, c.height);
-    context.drawImage(img, zX, zY);
+    context.drawImage(content, zX, zY, zW, zH);
 }
 function moveUp() {
-    zY -= 10;
-    animate();
+    if (canMoveImage(playerOne, 'up')) {
+        zY -= unit;
+        animate(playerOne);
+    }
 }
 function moveDown() {
-    zY += 10;
-    animate();
+    if (canMoveImage(playerOne, 'down')) {
+        zY += unit;
+        animate(playerOne);
+    }
 }
 function moveLeft() {
-    zX -= 10;
-    animate();
+    if (canMoveImage(playerOne, 'left')) {
+        zX -= unit;
+        animate(playerOne);
+    }
 }
 function moveRight() {
-    zX += 10;
-    animate();
+    if (canMoveImage(playerOne, 'right')) {
+        zX += unit;
+        animate(playerOne);
+    }
+}
+function canMoveImage(image, direction) {
+    switch (direction) {
+        case 'up':
+            return zY > 0;
+        case 'down':
+            return (zY + zH) < c.height;
+        case 'left':
+            return zX > 0;
+        case 'right':
+            return (zX + zW) < c.width;
+    }
+}
+function makeAWall() {
+    var img = new Image();
+    img.src = 'bricks.jpg';
+    img.addEventListener('load', function () {
+        context.drawImage(img, bX, bY, bW, bH);
+    });
 }
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,12 +1,30 @@
+//TODO measuring units in a grid
+//TODO generate random maze
+//TODO detect boundaries for user
+//TODO detect boundaries for zombies
+//TODO logic for zombie wandering
+//TODO end game on zombie contact
+//TODO OPTIONAL retrieving tincutures or cures or vaccines
+//TODO OPTIONAL refactor to class
+
+var unit = 50;
 var c = <HTMLCanvasElement>document.getElementById('myCanvas');
 let context = c.getContext('2d');
-let img = new Image();
+let playerOne = <HTMLImageElement> new Image();
+var zW = unit;
+var zH = unit;
 var zX = 0;
 var zY = 0;
-img.onload = animate;
-img.src = "zombie.gif";
-img.addEventListener('load', () => {
-  context.drawImage(img, zX, zY);
+var bW = unit;
+var bH = unit;
+var bX = c.width - bW;
+var bY = c.height - bH;
+playerOne.src = "zombie.gif";
+playerOne.width = zW;
+playerOne.height = zH;
+playerOne.addEventListener('load', () => {
+  context.drawImage(playerOne, zX, zY, zW, zH);
+  makeAWall();
 });
 
 document.addEventListener('keydown', (event) => {
@@ -26,27 +44,58 @@ document.addEventListener('keydown', (event) => {
   }
 }, true);
 
-function animate() {
+function animate(content) {
+  //TODO PASS IN CONFIG INSTEAD OF ONE THING
+  //EVERYTHING GETS REDRAWN?
   context.clearRect(0, 0, c.width, c.height);  // clear canvas
-  context.drawImage(img, zX, zY);
+  context.drawImage(content, zX, zY, zW, zH);
 }
 
 function moveUp() {
-  zY -= 10;
-  animate();
+  if (canMoveImage(playerOne, 'up')) {
+    zY -= unit;
+    animate(playerOne);
+  }
 }
 
 function moveDown() {
-  zY += 10;
-  animate();
+  if (canMoveImage(playerOne, 'down')) {
+    zY += unit;
+    animate(playerOne);
+  }
 }
 
 function moveLeft() {
-  zX -= 10;
-  animate();
+  if (canMoveImage(playerOne, 'left')) {
+    zX -= unit;
+    animate(playerOne);
+  }
 }
 
 function moveRight() {
-  zX += 10;
-  animate();
+  if (canMoveImage(playerOne, 'right')) {
+    zX += unit;
+    animate(playerOne);
+  }
+}
+
+function canMoveImage(image:HTMLImageElement, direction:string) {
+  switch(direction) {
+    case 'up':
+      return zY > 0;
+    case 'down':
+      return (zY + zH) < c.height;
+    case 'left':
+      return zX > 0;
+    case 'right':
+      return (zX + zW) < c.width;
+  }
+}
+
+function makeAWall() {
+  let img = new Image();
+  img.src = 'bricks.jpg';
+  img.addEventListener('load', () => {
+    context.drawImage(img, bX, bY, bW, bH);
+  });
 }
